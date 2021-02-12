e499980ab97a5dc96dbdb9aca9f21f008074fb8e
EdwardTatchim <77943456+EdwardTatchim@users.noreply.github.com>
2021-02-12 16:05:43 -0500

    Final lab3 edits

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,28 +1,25 @@
 /* Put your javascript in here */
-
-const leftButton = document.querySelector('button.arrow.left');
-const rightButton = document.querySelector('button.arrow.right');
-const ul = document.querySelector('div.images > ul');
-const liWidth = document.querySelector('div.galery > ul > li').clientWidth;
-const limit = ul.querySelectorAll('img').length*liWidth;
-
-const count=3;
-
-let location = 0;
-
-console.log(ul);
-
-const scrollFn = (isNext) => {
-
-    const gone = location + (count * (isNext ? liWidth : -(liWidth)));
-    console.log(gone)
-    if (gone < 0 || gone > limit){
+const btnPrev = document.querySelector('button.arrow#prev');
+    const btnNext = document.querySelector('button.arrow#next');
+    const ul = document.querySelector('div.galery > ul');
+    const liWidth = document.querySelector('div.galery#pics > ul >li').clientWidth;
+    const limit = ul.querySelectorAll('ul img').length * liWidth; 
+    
+    const count = 3;
+    let position = 0;
+    
+    console.log(ul); 
+    
+    const scrollFn = (isNext) => {
+      const off = position + (count * (isNext ? liWidth : -(liWidth)));
+      console.log(off);
+      if(off < 0 || off > limit) {
         return;
-    }
-
-    location = gone;
-    ul.style.transform = `translateX(${-(position)}px)`;
-};
-
-leftButton.onClick = () => scrollFn(false);
-rightButton.onClick = () => scrollFn(true);
+      }
+      
+      position = off;
+      ul.style.transform = `translateX(${-(position)}px)`;
+    };
+    
+    btnPrev.onclick = () => scrollFn(false);
+    btnNext.onclick = () => scrollFn(true);
